98d58e2d5afc20be57d0547ff3520ac5e563fe7a
dragonmux <git@dragonmux.network>
2022-08-18 10:45:40 +0100

    Integrated the UI stub with specter's main()

--- src/specter.cxx
+++ src/specter.cxx
@@ -1,9 +1,11 @@
 // SPDX-License-Identifier: BSD-3-Clause
 #include <QtWidgets/QApplication>
+#include "mainWindow.hxx"
 
 int main(int argCount, char **argList)
 {
 	QApplication app{argCount, argList};
-
-	//return app.exec();
+	Specter specter{};
+	specter.show();
+	return app.exec();
 }
